8551b3bde27f1dd54c14e14c465de531b048d2b1
Marcelo venancio <marcelovenancio100@users.noreply.github.com>
2019-07-16 11:49:13 -0300

    Update public-api.ts

--- projects/auth-oauth2/src/public-api.ts
+++ projects/auth-oauth2/src/public-api.ts
@@ -5,3 +5,7 @@
 export * from './lib/auth-oauth2.service';
 export * from './lib/auth-oauth2.component';
 export * from './lib/auth-oauth2.module';
+export * from './lib/auth-oauth2.guard';
+export * from './lib/token/token-interceptor';
+export * from './lib/components/login/login.component';
+export * from './lib/components/logout/logout.component';
